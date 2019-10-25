084551c9de043c3492c97e392bb8504cd945617f
dgw <dgw@technobabbl.es>
2019-10-25 02:45:14 -0500

    Implement mocking by nick (most recent non-action line)

--- spongemock.py
+++ spongemock.py
@@ -7,17 +7,92 @@ Copyright 2019 dgw, technobabbl.es
 from __future__ import print_function, division, unicode_literals, absolute_import
 
 import random
+import re
 
-from sopel import module
+from sopel import module, tools
+
+
+def setup(bot):
+    if 'mock_lines' not in bot.memory:
+        bot.memory['mock_lines'] = tools.SopelMemory()
+
+
+def shutdown(bot):
+    try:
+        del bot.memory['mock_lines']
+    except KeyError:
+        pass
+
+
+@module.echo
+@module.rule('(.*)')
+@module.priority('low')
+@module.require_chanmsg
+def cache_lines(bot, trigger):
+    if trigger.sender not in bot.memory['mock_lines']:
+        bot.memory['mock_lines'][trigger.sender] = tools.SopelMemory()
+
+    line = trigger.group()
+    # don't store /me commands, or obvious bot commands
+    if not line.startswith('\x01ACTION') and not re.match(bot.config.core.prefix, line):
+        bot.memory['mock_lines'][trigger.sender][trigger.nick] = line
+
+
+@module.echo
+@module.event('PART')
+@module.priority('low')
+def part_prune(bot, trigger):
+    if trigger.nick == bot.nick:
+        # We left; clean up everything cached for that channel.
+        bot.memory['mock_lines'].pop(trigger.sender, None)
+    else:
+        # Someone else left; remove their cache entry.
+        bot.memory['mock_lines'].get(trigger.sender, {}).pop(trigger.nick, None)
+
+
+@module.echo
+@module.event('QUIT')
+@module.priority('low')
+def quit_prune(bot, trigger):
+    for channel in bot.memory['mock_lines'].keys():
+        bot.memory['mock_lines'][channel].pop(trigger.nick, None)
+
+
+@module.echo
+@module.event('KICK')
+@module.priority('low')
+def kick_prune(bot, trigger):
+    if trigger.nick == bot.nick:
+        # We were kicked; clean up everything cached for that channel.
+        bot.memory['mock_lines'].pop(trigger.sender, None)
+    else:
+        # Some other poor sod (or spammer) got kicked; remove their cache entry.
+        bot.memory['mock_lines'].get(trigger.sender, {}).pop(trigger.nick, None)
+
+
+def get_cached_line(bot, channel, nick):
+    line = bot.memory['mock_lines'].get(channel, {}).get(nick, '')
+    if line:
+        return '<{}> {}'.format(nick, line)
 
 
 @module.commands('spongemock')
+@module.example('.spongemock Fortnite is the best game ever!')
 def spongemock(bot, trigger):
+    """Make sPonGeMoCk text from the input (or the last thing a user said)."""
     if not trigger.group(2):
-        bot.reply('I need text!')
+        bot.reply('I need text, or a nickname!')
         return module.NOLIMIT
 
-    bot.say(mock_case(trigger.group(2)))
+    line = get_cached_line(bot, trigger.sender, trigger.group(3))
+
+    if line:
+        # last thing someone else said
+        nick, text = line.split(maxsplit=1)
+        bot.say(' '.join([nick, mock_case(text)]))
+    else:
+        # use given text
+        bot.say(mock_case(trigger.group(2)))
 
 
 def mock_case(text):
